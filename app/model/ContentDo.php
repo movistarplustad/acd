32013ba47e689b361a4153b8681e602810dd6f03
Fernando García Gómez <fernando.garciagomez.01@telefonica.com>
2019-11-11 16:52:58 +0100

    Tener disponible el timestamp de grabación de un contenido

--- app/model/ContentDo.php
+++ app/model/ContentDo.php
@@ -1,8 +1,11 @@
 <?php
+
 namespace Acd\Model;
 
-class ContentKeyInvalidException extends \exception {}
-class ContentDoException extends \exception {}
+class ContentKeyInvalidException extends \exception
+{ }
+class ContentDoException extends \exception
+{ }
 class ContentDo
 {
 	const PERIOD_OF_VALIDITY_START = 'start';
@@ -15,6 +18,7 @@ class ContentDo
 	private $title;
 	private $periodOfValidity;
 	private $aliasId;
+	private $saveTime;
 	private $tags;
 	private $profile;
 	//private $data; /* Array key/value of variable fields */
@@ -23,12 +27,14 @@ class ContentDo
 	private $countParents; /* Number of parent, used in content editor for info */
 	private $countAliasId; /* Number aliad_id in all contents, used in content editor for info */
 
-	public function __construct() {
+	public function __construct()
+	{
 		$this->id = null;
 		$this->idStructure = null;
 		$this->periodOfValidity = array(
 			ContentDo::PERIOD_OF_VALIDITY_START => -INF,
-			ContentDo::PERIOD_OF_VALIDITY_END => INF);
+			ContentDo::PERIOD_OF_VALIDITY_END => INF
+		);
 		$this->aliasId = null;
 		$this->tags = array();
 		$this->profile = new FieldDo();
@@ -38,32 +44,41 @@ class ContentDo
 		$this->parent = null;
 	}
 	/* Setters and getters attributes */
-	public function setId($id) {
-		$this->id = (string)$id;
+	public function setId($id)
+	{
+		$this->id = (string) $id;
 	}
-	public function getId() {
+	public function getId()
+	{
 		return $this->id;
 	}
-	public function setIdStructure($idStructure) {
-		$this->idStructure = (string)$idStructure;
+	public function setIdStructure($idStructure)
+	{
+		$this->idStructure = (string) $idStructure;
 	}
-	public function getIdStructure() {
+	public function getIdStructure()
+	{
 		return $this->idStructure;
 	}
-	public function setTitle($title) {
+	public function setTitle($title)
+	{
 		$this->title = $title;
 	}
-	public function getTitle() {
+	public function getTitle()
+	{
 		return $this->title;
 	}
-	private function checkExpirityAttribute($attributeName) {
+	private function checkExpirityAttribute($attributeName)
+	{
 		return ($attributeName === ContentDo::PERIOD_OF_VALIDITY_START || $attributeName === ContentDo::PERIOD_OF_VALIDITY_END);
 	}
-	public function setPeriodOfValidity($periodOfValidity) {
+	public function setPeriodOfValidity($periodOfValidity)
+	{
 		// $attributeName acepted PERIOD_OF_VALIDITY_START | PERIOD_OF_VALIDITY_END
-		$this->periodOfValidity= $periodOfValidity;
+		$this->periodOfValidity = $periodOfValidity;
 	}
-	public function getPeriodOfValidity($attributeName = ContentDo::PERIOD_OF_VALIDITY_RAW) {
+	public function getPeriodOfValidity($attributeName = ContentDo::PERIOD_OF_VALIDITY_RAW)
+	{
 		// TODO. Future period_of_validity class
 		switch ($attributeName) {
 			case ContentDo::PERIOD_OF_VALIDITY_RAW:
@@ -81,14 +96,14 @@ class ContentDo
 			default:
 				if ($this->checkExpirityAttribute($attributeName)) {
 					return isset($this->periodOfValidity[$attributeName]) ? $this->periodOfValidity[$attributeName] : null;
-				}
-				else {
+				} else {
 					throw new ContentDoException("Unknown period of validity attibute [$attributeName]", 1);
 				}
 				break;
 		}
 	}
-	public function checkValidityDate($date) {
+	public function checkValidityDate($date)
+	{
 		$inDate = true;
 		$start = $this->getPeriodOfValidity(ContentDo::PERIOD_OF_VALIDITY_START) ? $this->getPeriodOfValidity(ContentDo::PERIOD_OF_VALIDITY_START) : -INF;
 		$end = $this->getPeriodOfValidity(ContentDo::PERIOD_OF_VALIDITY_END) ? $this->getPeriodOfValidity(ContentDo::PERIOD_OF_VALIDITY_END) : INF;
@@ -97,88 +112,102 @@ class ContentDo
 		}
 		return $inDate;
 	}
-	public function checkProfile($profile) {
+	public function checkProfile($profile)
+	{
 		$profilesContent = $this->getProfile()->getValue();
 		// empty profile or content un-profiled
-		if ($profile == ''  || count($profilesContent ) == 0) {
+		if ($profile == ''  || count($profilesContent) == 0) {
 			$profileAllowed = true;
-		}
-		else {
+		} else {
 			$profileAllowed = in_array($profile, $this->getProfile()->getValue());
 		}
 
 		return $profileAllowed;
 	}
-	public function setAliasId($aliasId) {
+	public function setAliasId($aliasId)
+	{
 		$this->aliasId = $aliasId;
 	}
-	public function getAliasId() {
+	public function getAliasId()
+	{
 		return $this->aliasId;
 	}
-	public function setTags($tags) {
+	public function setTags($tags)
+	{
 		if (is_array($tags)) {
 			$this->tags = $tags;
-		}
-		else {
+		} else {
 			throw new ContentDoException("Input data should be an array", 1);
 		}
 	}
-	public function setProfile($profile) {
+	public function setProfile($profile)
+	{
 		$this->profile = $profile;
 	}
-	public function setProfileValues($aProfile) {
+	public function setProfileValues($aProfile)
+	{
 		$this->profile->setValue($aProfile);
 	}
-	public function getProfile() {
+	public function getProfile()
+	{
 		return $this->profile;
 	}
-	public function getTags() {
+	public function getTags()
+	{
 		return $this->tags;
 	}
-	public function addField($field) {
+	public function addField($field)
+	{
 		$this->getFields()->add($field);
 	}
-	public function getFields() {
+	public function getFields()
+	{
 		return $this->fields;
 	}
-	private function setFields($fields) {
+	private function setFields($fields)
+	{
 		foreach ($fields as $field) {
 			$this->fields->add(clone $field);
 		}
 	}
-	public function getFieldType($fieldName) {
+	public function getFieldType($fieldName)
+	{
 		try {
 			return $this->getFields()->getType($fieldName);
 		} catch (KeyInvalidException $e) {
 			return '';
 		}
 	}
-	public function getFieldValue($fieldName) {
+	public function getFieldValue($fieldName)
+	{
 		try {
 			return $this->getFields()->getValue($fieldName);
 		} catch (KeyInvalidException $e) {
 			return '';
 		}
 	}
-	public function setFieldValue($fieldName, $value) {
+	public function setFieldValue($fieldName, $value)
+	{
 		$this->getFields()->setValue($fieldName, $value);
 	}
-	public function setStructureRef($fieldName, $structureRef) {
+	public function setStructureRef($fieldName, $structureRef)
+	{
 		$this->getFields()->setStructureRef($fieldName, $structureRef);
 	}
-	public function setData($keyData, $data = null) {
+	public function setData($keyData, $data = null)
+	{
 		echo "TODO";
 		/* Setting full structure */
 		if ($data === null) {
 			// TODO errores si campo existe y tipo dato válido
 			$this->data = $keyData;
 		}
-		/* Set key / value */
-		else {
+		/* Set key / value */ else {
 			$this->data[$keyData] = $data;
 		}
 	}
-	public function getData($key = null) {
+	public function getData($key = null)
+	{
 		$data = array();
 		foreach ($this->getFields() as $field) {
 			$data[$field->getId()] = $field->getValue();
@@ -186,34 +215,43 @@ class ContentDo
 
 		return $data;
 	}
-	public function setParent($parent) {
+	public function setParent($parent)
+	{
 		$this->parent = $parent; // ContentDO
 	}
-	public function getParent() {
+	public function getParent()
+	{
 		return $this->parent;
 	}
-	public function setCountParents($countParents) {
+	public function setCountParents($countParents)
+	{
 		$this->countParents = $countParents;
 	}
-	public function getCountParents() {
+	public function getCountParents()
+	{
 		return $this->countParents;
 	}
-	public function setCountAliasId($countAliasId) {
+	public function setCountAliasId($countAliasId)
+	{
 		$this->countAliasId = $countAliasId;
 	}
-	public function getCountAliasId() {
+	public function getCountAliasId()
+	{
 		return $this->countAliasId;
 	}
 
 	// With the data structure, build the skeleton of content
-	public function buildSkeleton($structure) {
+	public function buildSkeleton($structure)
+	{
 		$this->setIdStructure($structure->getId());
 		$this->setFields($structure->getFields());
 	}
-	private function isFilledRef($value) {
+	private function isFilledRef($value)
+	{
 		return (isset($value[0]['ref']) && $value[0]['ref']);
 	}
-	public function load($rawData, $structure = null) {
+	public function load($rawData, $structure = null)
+	{
 		$this->setId($rawData['id']);
 		$this->setTitle($rawData['title']);
 		$periodOfValidity = isset($rawData['period_of_validity']) ? $rawData['period_of_validity'] : [];
@@ -224,11 +262,12 @@ class ContentDo
 		$this->setTags($tags);
 		$profile = isset($rawData['profile']) ? $rawData['profile'] : [];
 		$this->setProfileValues($profile);
-		if($structure !== null) {
+		$this->setSaveTime($rawData['save_ts']);
+		if ($structure !== null) {
 			$this->buildSkeleton($structure);
 		}
 		$fields = $this->getFields();
-		if (is_array($rawData['data'])){
+		if (is_array($rawData['data'])) {
 			foreach ($rawData['data'] as $key => $value) {
 				$this->getFields()->setValue($key, $value);
 				switch ($this->getFields()->get($key)->getType()) {
@@ -242,10 +281,11 @@ class ContentDo
 			}
 			//d($this->getFields());
 		}
-//+d($this);
+		//+d($this);
 		//$this->setFields($fields);
 	}
-	public function tokenizeData() {
+	public function tokenizeData()
+	{
 		$aFieldsData = array();
 		// Test if the field value is a real value or reference
 		foreach ($this->getFields() as $field) {
@@ -271,6 +311,7 @@ class ContentDo
 		return  array(
 			'id' => $this->getId(),
 			'id_structure' => $this->getIdStructure(),
+			'save_ts' => $this->getSaveTime(),
 			'title' => $this->getTitle(),
 			'period_of_validity' => $this->getPeriodOfValidity(ContentDo::PERIOD_OF_VALIDITY_TOKENIZE),
 			'alias_id' => $this->getAliasId(),
@@ -278,5 +319,25 @@ class ContentDo
 			'profile' => $this->getProfile()->getValue(),
 			'data' => $aFieldsData
 		);
+	}
+
+	/**
+	 * Get the value of saveTime
+	 */
+	public function getSaveTime()
+	{
+		return $this->saveTime;
+	}
+
+	/**
+	 * Set the value of saveTime
+	 *
+	 * @return  self
+	 */
+	public function setSaveTime($saveTime)
+	{
+		$this->saveTime = $saveTime;
+
+		return $this;
 	}
 }
